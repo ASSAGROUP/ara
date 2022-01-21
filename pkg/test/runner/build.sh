a60616202af076b8028a68756e240be96f9c1c43
Shashi Bhushan Rai <shashi.rai@bhojpur-consulting.org>
2022-01-21 11:44:05 +0530

    test runner build

--- pkg/test/runner/build.sh
+++ pkg/test/runner/build.sh
@@ -4,6 +4,7 @@ set -e
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o bin/runner_linux_amd64 main.go
 curl -L https://github.com/upx/upx/releases/download/v3.96/upx-3.96-amd64_linux.tar.xz | tar xJ
 upx-3.96-amd64_linux/upx bin/runner_linux_amd64 
+sudo chmod 764 bin/runner_linux_amd64
 rm -r upx-3.96-amd64_linux
 go install github.com/GeertJohan/go.rice/rice@v1.0.2
 RICEBIN="$GOBIN"
